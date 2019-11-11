5ab61eb43b11883b7a42cc96c36290e5282650d0
adargut <adar276@gmail.com>
2019-11-11 14:24:30 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OSClass.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OSClass.dir/main.c.obj"
+  "OSClass.exe"
+  "OSClass.exe.manifest"
+  "OSClass.pdb"
+  "libOSClass.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OSClass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/OSClass.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OSClass.
+# This may be replaced when dependencies are built.
