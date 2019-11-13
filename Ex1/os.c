d9a48b3f56aabf4de8d830b2bbcb235aee6f39f2
adargut <adar276@gmail.com>
2019-11-13 17:05:57 +0200

    Bug fixes in os.c

--- Ex1/os.c
+++ Ex1/os.c
@@ -51,11 +51,11 @@ int main(int argc, char **argv)
 {
     uint64_t pt = alloc_page_frame();
 
-    //assert(page_table_query(pt, 0xcafe) == NO_MAPPING);
-    //page_table_update(pt, 0xcafe, 0xf00d);
-    //assert(page_table_query(pt, 0xcafe) == 0xf00d);
-    //page_table_update(pt, 0xcafe, NO_MAPPING);
-    //assert(page_table_query(pt, 0xcafe) == NO_MAPPING);
+    assert(page_table_query(pt, 0xcafe) == NO_MAPPING);
+    page_table_update(pt, 0xcafe, 0xf00d);
+    assert(page_table_query(pt, 0xcafe) == 0xf00d);
+    page_table_update(pt, 0xcafe, NO_MAPPING);
+    assert(page_table_query(pt, 0xcafe) == NO_MAPPING);
 
     return 0;
 }
