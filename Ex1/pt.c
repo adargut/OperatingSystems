df1a7a849138835b0679b8bbfed27799cae69b7c
adargut <adar276@gmail.com>
2019-11-16 09:51:33 -0800

    PRogress

--- Ex1/pt.c
+++ Ex1/pt.c
@@ -4,25 +4,8 @@
 #include "stdlib.h"
 #include "inttypes.h"
 #include "string.h"
+#include "limits.h"
 
-// Define a node in a trie
-typedef struct trie_node
-{
-    uint64_t key; // 9 bit key by which we perform search
-    uint64_t children [512]; // 2^9 possibilities for node children
-    uint64_t ppn; // ppn mapping found in leaf node
-    bool isLeaf; // flag to distinguish leaf nodes from inner nodes
-} TrieNode;
-
-// Store a list of all base addresses for pts
-typedef struct base_list
-{
-    uint64_t base_addr;
-    TrieNode *base_ptr;
-    struct base_list *next;
-} BaseList;
-
-static BaseList *root_list;
 
 // Util function to reverse string
 char *strrev(char *str){
@@ -68,60 +51,47 @@ char *decToBinary(long n)
     return binary;
 }
 
-
-// Util function to fetch root address from list of roots
-TrieNode *getRoot(uint64_t root_addr) {
-    BaseList *head = root_list;
-
-    while (head != NULL) {
-        if (head->base_addr == root_addr) {
-            return head->base_ptr;
-        }
-        head = head->next;
+void print_bin(uint64_t integer)
+{
+    int i = CHAR_BIT * sizeof (uint64_t); /* however many bits are in an integer */
+    while(i--) {
+        putchar('0' + ((integer >> i) & 1));
     }
-    fprintf(stderr, "root for base page table register not found");
-}
-
-void addRoot(uint64_t root_addr) {
-    BaseList *head = root_list;
-
-    while (head->next != NULL) {
-        head = head->next;
-    }
-//    head->next = malloc(sizeof())
 }
 
 // Create or destroy a mapping in the page table
 void page_table_update(uint64_t pt, uint64_t vpn, uint64_t ppn) {
-    printf("hello\n");
-    printf("\n");
+//    void *base = pto_virt(pt);
+    printf("at base");
+}
+
+/* Function to reverse bits of num */
+uint64_t reverseBits(uint64_t num)
+{
+    unsigned int  NO_OF_BITS = sizeof(num) * 8;
+    uint64_t reverse_num = 0, i, temp;
+
+    for (i = 0; i < NO_OF_BITS; i++)
+    {
+        temp = (num & (1 << i));
+        if(temp)
+            reverse_num |= (1 << ((NO_OF_BITS - 1) - i));
+    }
+
+    return reverse_num;
 }
 
 // Query a vpn from page table: return ppn if exists, NO_MAPPING else
 uint64_t page_table_query(uint64_t pt, uint64_t vpn)
 {
-    char vpn_buff[45], *ptr;
-    int i = 0;
-//    sprintf(vpn_buff, "%" PRIu64, vpn);
-//    printf("got bin vpn %s\n", vpn_buff);
-//    uint64_t x = strtol(vpn_buff, &ptr, 10);
-//    printf("x is now %lx\n", x);
-    ptr = decToBinary(vpn);
-    printf("res is %s\n", ptr);
-    printf("length of res is %d\n", strlen(ptr));
-    while (i <= 44) {
-        vpn_buff[i++] = '0';
-    }
-    vpn_buff[45] = '\0';
-    vpn_buff[0] = '0';
-    printf("vpn buff is %s\n", vpn_buff);
-    printf("vpn[0] is %c\n", vpn_buff[0]);
-    int start_idx = strlen(vpn_buff) - strlen(ptr);
-    printf("start from %d\n", start_idx);
-    int j = 0;
-    for (int i = start_idx; i < 45; i++) {
-        printf("hey, ptr is %d\n", ptr[j]);
-        vpn_buff[i] = ptr[j];
-        j++;
+    uint64_t mask = 0b0000000000000000000111111111000000000000000000000000000000000000;
+    print_bin(mask);
+    printf("\n");
+    int depth = 0;
+    uint64_t *node_ptr = (uint64_t *)phys_to_virt(pt);
+
+    while (++depth < 5) {
+        uint64_t page_offset = mask & vpn;
+
     }
 }
