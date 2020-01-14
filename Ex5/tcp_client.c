4628deff470102ed04143355a09f0f1e18195512
adargut <adar276@gmail.com>
2020-01-14 14:45:45 -0800

    progress on ex5

--- Ex5/tcp_client.c
+++ Ex5/tcp_client.c
@@ -22,6 +22,8 @@
 
 #include <assert.h>
 
+#include <stdbool.h>
+
 off_t get_file_length(FILE * file) {
     fpos_t position; // fpos_t may be a struct and store multibyte info
     off_t length; // off_t is integral type, perhaps long long
@@ -39,14 +41,17 @@ off_t get_file_length(FILE * file) {
 // MINIMAL ERROR HANDLING FOR EASE OF READING
 
 int main(int argc, char * argv[]) {
-    int totalsent = -1;
+    int totalsent = 0;
     int sockfd = -1;
+    int nread = -1;
     int bytes_sent = 0;
     int N = 0;
     int port = atoi(argv[2]);
     char * ip = argv[1];
     char * path = argv[3];
-    char send_buff[1024];
+    char send_buff[2500];
+    char N_buff[10];
+    char serv_resp[1024] = { 0 };
     FILE * file;
 
     struct sockaddr_in serv_addr; // where we Want to get to
@@ -59,29 +64,33 @@ int main(int argc, char * argv[]) {
         return 1;
     }
 
-    file = fopen(path, "r");
+    file = fopen(path, "rb");
     if (file == NULL) {
         printf("Error : Opening file failed \n");
         return 1;
     }
 
-//      for (char c = getc(file); c != EOF; ) {
-          // Increment count of chars
-//          N++;
-//          if (N > 100) {
-//              break;
-//          }
-//      }
-
-//      N = get_file_length(file);
-
-    printf("Client got N: %d\n", N);
+    memset(send_buff, 0, sizeof(N_buff));
+    N = get_file_length(file);
+    N_buff[0] = ntohl(N);
+    N_buff[1] = "\0";
+    printf("client got N: %d\n", N);
 
     memset(send_buff, 0, sizeof(send_buff));
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+
+
+    while (fgets(send_buff, 1024, file) != NULL)
+        printf(" %s ", send_buff);
+
+
+      printf("client got msg:%s\n",send_buff);
+
+
+      if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         printf("\n Error : Could not create socket \n");
         return 1;
     }
+    printf("Client sending: %s\n", send_buff);
 
     // print socket details
     getsockname(sockfd,
@@ -115,10 +124,17 @@ int main(int argc, char * argv[]) {
            inet_ntoa((my_addr.sin_addr)), ntohs(my_addr.sin_port),
            inet_ntoa((peer_addr.sin_addr)), ntohs(peer_addr.sin_port));
 
+
     int notwritten = strlen(send_buff);
 
-    // keep looping until nothing left to read
+    // Write N from client to server
+    bytes_sent = write(sockfd,
+                        N_buff,
+                        4);
+
+    // Write message from client to server
     while (notwritten > 0) {
+        printf("client writing message...\n");
         // notwritten = how much we have left to write
         // totalsent  = how much we've written so far
         // nsent = how much we've written in last write() call */
@@ -132,19 +148,19 @@ int main(int argc, char * argv[]) {
         totalsent += bytes_sent;
         notwritten -= bytes_sent;
     }
-    // send data to server
-    //  while( 1 )
-    //  {
-    //    bytes_sent = write(sockfd,
-    //                       send_buff,
-    //                       sizeof(send_buff) - 1);
-    //    if( bytes_sent <= 0 )
-    //      break;
-    //    send_buff[bytes_sent] = '\0';
-    //    puts( send_buff );
-    //  }
+
+    // Read response from the server
+    while (1) {
+        printf("hi\n");
+        nread = read(sockfd,
+                     serv_resp,
+                     sizeof(serv_resp) - 1);
+        printf("client got response from server: %d\n", serv_resp[0]);
+        if (nread > 0) break;
+    }
 
     close(sockfd); // is socket really done here?
+    close(file);
     //printf("Write after close returns %d\n", write(sockfd, recv_buff, 1));
     return 0;
 }
--- Ex5/tcp_server.c
+++ Ex5/tcp_server.c
@@ -20,7 +20,11 @@
 
 #include <assert.h>
 
+#include <ctype.h>
+
 // MINIMAL ERROR HANDLING FOR EASE OF READING
+
+static int total_PCC[94] = { 0 };
 
 int main(int argc, char * argv[]) {
     int totalsent = -1;
@@ -36,9 +40,13 @@ int main(int argc, char * argv[]) {
     socklen_t addrsize = sizeof(struct sockaddr_in);
 
     char data_buff[1024];
-    time_t ticks;
+    int N_buff[2];
+    int serv_resp[100];
 
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    int yes = 1;
+
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
     memset( & serv_addr, 0, addrsize);
 
     serv_addr.sin_family = AF_INET;
@@ -79,22 +87,57 @@ int main(int argc, char * argv[]) {
                inet_ntoa(peer_addr.sin_addr),
                ntohs(peer_addr.sin_port),
                inet_ntoa(my_addr.sin_addr),
-               ntohs(my_addr.sin_port));
+               ntohl(my_addr.sin_port));
 
-        // write time
-        ticks = time(NULL);
-        snprintf(data_buff, sizeof(data_buff),
-                 "%.24s\r\n", ctime( & ticks));
+
 
         while (1) {
             nread = read(connfd,
+                         N_buff,
+                         4);
+            printf("server got N %d\n", N_buff[0]);
+            printf("what is nread? %d\n", nread);
+            if (nread > 0) break;
+        }
+
+        uint32_t printable = 0;
+
+        while (1) {
+            printf("hiya\n");
+            nread = read(connfd,
                          data_buff,
-                         sizeof(data_buff) - 1);
+                         100);
             if (nread <= 0)
                 break;
-            data_buff[nread] = '\0';
-            puts(data_buff);
+            data_buff[nread + 1] = '\0';
+            printf("what did server read? %s\n", data_buff);
+            printf("server read %d\n", nread);
+            for (int i = 0; i < nread; i++) {
+                if (isprint(data_buff[i])) {
+                    total_PCC[data_buff[i]]++;
+                    printable++;
+                    printf("what is it? %d\n", data_buff[i]);
+                }
+            }
+            char *the_msg = malloc(300);
+            strcat(the_msg, "# of printable characters: ");
+            char str[300];
+            sprintf(str, "%d", printable);
+            printf("what is str? %s\n", str);
+            strcat(the_msg, str);
+            printf("server sent response: %s\n", the_msg);
+            printf("the total #of chars: %d\n", strlen(data_buff));
+            break;
         }
+
+        serv_resp[0] = printable;
+
+        nread = write(connfd,
+                           serv_resp,
+                      sizeof(serv_resp) - 1);
+        // check if error occured (client closed connection?)
+        assert(nread >= 0);
+        printf("server sent: %d\n", serv_resp[0]);
         // close socket
         close(connfd);
     }
