9b5847a26fee943fbc1a2e9d2c47c116e80c56a7
adargut <adar276@gmail.com>
2020-01-25 23:48:35 +0200

    mistake

--- Ex5/pcc_server.c
+++ Ex5/pcc_server.c
@@ -1,184 +1,71 @@
-#define _DEFAULT_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/socket.h>
-#include <sys/types.h>
+
 #include <netinet/in.h>
-#include <netdb.h>
+
 #include <arpa/inet.h>
-#include <stdint.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <unistd.h>
+
 #include <errno.h>
+
 #include <string.h>
+
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <signal.h>
 
-uint32_t char_statistics[127];
-int canceled = 0;
+#include <time.h>
 
-uint32_t get_N(int fd, uint32_t* result)
-{
-    uint32_t N;
-    char *bytes = (char*)&N;
-    int left = sizeof(uint32_t);
-    int bytes_read;
-    while (left > 0)
-    {
-        bytes_read = read(fd, bytes, left);
-        if (bytes_read < 0)
-        {
-            if (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE)
-            {
-                perror("Error: ");
-                return 1;
-            }
-            else
-            {
-                perror("Error: ");
-                exit(1);
-            }
-        }
-        else
-        {
-            bytes += bytes_read;
-            left -= bytes_read;
-        }
-    }
-    bytes-=sizeof(uint32_t);
-    *result = ntohl(*((uint32_t*)(bytes)));
-    return 0;
-}
+#include <assert.h>
 
-int get_message(int fd, char* message, uint32_t N)
-{
-    uint32_t bytes_read;
-    uint32_t left = N;
-    //read file contents to <message>
-    while(left > 0)
-    {
-        bytes_read = read(fd,
-                          message,
-                          left);
-        if( bytes_read < 0 )
-        {
-            if (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE)
-            {
-                perror("Error: ");
-                return 1;
-            }
-            else
-            {
-                perror("Error: ");
-                exit(1);
-            }
-        }
-        else
-        {
-            message += bytes_read;
-            left -= bytes_read;
-        }
-    }
-    return 0;
-}
+#include <ctype.h>
 
-uint32_t count_printable(char* message, uint32_t N)
-{
-    uint32_t printable = 0;
-    for (uint32_t i = 0; i < N; i++)
-    {
-        if(message[i] >= 32 && message[i] <= 126)
-        {
-            printable++;
-        }
-    }
-    return printable;
-}
+#include <stdbool.h>
 
-int send_printable(int fd, uint32_t printable)
-{
-    uint32_t temp = htonl(printable);
-    char *bytes = (char*)&temp;
-    int left = sizeof(uint32_t);
-    int bytes_written;
-    while (left > 0)
-    {
-        bytes_written = write(fd, bytes, left);
-        if (bytes_written < 0)
-        {
-            if (errno == ETIMEDOUT || errno == ECONNRESET || errno == EPIPE)
-            {
-                perror("Error: ");
-                return 1;
-            }
-            else
-            {
-                perror("Error: ");
-                exit(1);
-            }
-        }
-        else
-        {
-            bytes += bytes_written;
-            left -= bytes_written;
-        }
-    }
-    return 0;
-}
+#include <sys/signal.h>
 
-void update_statistics(char* message, uint32_t N)
-{
-    for (uint32_t i = 0; i < N; i++)
-    {
-        if(message[i] >= 32 && message[i] <= 126)
-        {
-            char_statistics[(int)(message[i])]++;
-        }
-    }
-}
+#define BUFF_LEN 32
 
-int handle_client(int fd)
-{
-    //printf("began handling client\n");
-    uint32_t N = 0;
-    if(get_N(fd, &N) != 0) return 0;
-    uint32_t printable = 0;
-    char* message = malloc(N);
-    if(get_message(fd, message, N) != 0)
-    {
-        free(message);
-        return 0;
-    }
-    printable = count_printable(message, N);
-    if(send_printable(fd, printable) != 0)
-    {
-        free(message);
-        return 0;
-    }
-    update_statistics(message, N);
-    free(message);
-    return 0;
-}
+#define RESP_LEN 256
 
-void print_statistics()
-{
-    for (char i = 32; i < 127; i++)
-    {
-        printf("char ’%c’ : %u times\n", i, char_statistics[(uint32_t)i]);
-    }
-}
+#define UNUSED(x)(void)(x)
+
+static bool busy = false;
+static bool cancelled = false;
+static unsigned int total_PCC[95] = {0};
 
 void handle_sigint(int signum, siginfo_t* info, void* ptr)
 {
-    canceled = 1;
+    cancelled = true;
 }
 
-int main(int argc, char** argv)
-{
-    //setup...
-    //register signal handler for SIG_INT
+int main(int argc, char * argv[]) {
+    int nread = -1;
+    int listenfd = -1;
+    int connfd = -1;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in my_addr;
+    struct sockaddr_in peer_addr;
+
+    UNUSED(peer_addr);
+    UNUSED(my_addr);
+
+    socklen_t addrsize = sizeof(struct sockaddr_in);
+
+    char data_buff[BUFF_LEN];
+
+    uint32_t N_buff[1];
+    uint32_t serv_resp[RESP_LEN];
+
+    if (argc != 2) {
+        printf("Error : Incorrect args count \n");
+        return 1;
+    }
+
+    // Define signal for SIGINT
     struct sigaction sigint_action;
     memset(&sigint_action, 0, sizeof(sigint_action));
     sigint_action.sa_sigaction = handle_sigint;
@@ -189,62 +76,98 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    int listenfd  = -1;
-    int connfd    = -1;
-    if (argc != 2)
-    {
-        fprintf(stderr, "Wrong number of arguments\n");
-        return -1;
+    // Socket
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror(strerror(errno));
+        exit(1);
     }
-    unsigned short port = (unsigned short)(atoi(argv[1]));
+    // Set socket options
+    memset( & serv_addr, 0, addrsize);
+    int yes = 1;
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, & yes, sizeof(yes));
 
-    socklen_t addrsize = sizeof(struct sockaddr_in );
-    struct sockaddr_in serv_addr;
-    listenfd = socket( AF_INET, SOCK_STREAM, 0 );
-    int enable = 1;
-    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
-        perror("setsockopt(SO_REUSEADDR) failed: ");
-    memset( &serv_addr, 0, addrsize );
-
+    // Set binding config
     serv_addr.sin_family = AF_INET;
     // INADDR_ANY = any local machine address
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(port);
+    serv_addr.sin_port = htons(atoi(argv[1]));
 
-    if( 0 != bind( listenfd,
-                   (struct sockaddr*) &serv_addr,
-                   addrsize ) )
-    {
-        fprintf(stderr, "Error : Bind Failed. %s \n", strerror(errno));
+    // Bind socket
+    if (0 != bind(listenfd,
+                  (struct sockaddr * ) & serv_addr,
+                  addrsize)) {
+        printf("\n Error : Bind Failed. %s \n", strerror(errno));
         return 1;
     }
 
-    if( 0 != listen( listenfd, 10 ) )
-    {
-        fprintf(stderr, "Error : Listen Failed. %s \n", strerror(errno));
+    // Listen on port
+    if (0 != listen(listenfd, 10)) {
+        printf("\n Error : Listen Failed. %s \n", strerror(errno));
         return 1;
     }
 
-    while( canceled == 0 )
-    {
+    while (!cancelled) {
         // Accept a connection.
-        connfd = accept( listenfd,
-                         NULL,
-                         NULL);
-
-        //printf("accepted connection\n");
-        if( connfd < 0 )
-        {
-            if(errno != EINTR)
-                printf("\n Error : Accept Failed. %s \n", strerror(errno));
+        connfd = accept(listenfd,
+                        (struct sockaddr * ) & peer_addr, & addrsize);
+        // Server now handling connection
+        busy = true;
+        // Do not accept new connections after signal
+        if (cancelled) break;
+        if (connfd < 0) {
+            printf("\n Error : Accept Failed. %s \n", strerror(errno));
+            return 1;
         }
-        else
-        {
-            handle_client(connfd);
-            close(connfd);
+
+        // Read N value from client
+        nread = read(connfd,
+                     N_buff,
+                     sizeof(int));
+        uint32_t printable = 0;
+
+        // Read file from client
+        int total_read = 0;
+        while (total_read < N_buff[0]) {
+            nread = read(connfd,
+                         data_buff,
+                         BUFF_LEN);
+            total_read += nread;
+            if (nread <= 0)
+                break;
+            data_buff[nread] = '\0';
+            for (int i = 0; i < nread; i++) {
+                // Update printable histogram
+                if (data_buff[i] <= 126 && data_buff[i] >= 32) {
+                    total_PCC[data_buff[i] - 32]++;
+                    printable++;
+                }
+            }
+            // Check if we finished reading client request
+            if (total_read == N_buff[0]) {
+                break;
+            }
+        }
+
+        // Set response
+        serv_resp[0] = printable;
+        // Write response to client
+        nread = write(connfd,
+                      serv_resp,
+                      sizeof(serv_resp) - 1);
+        // Set to non busy
+        busy = false;
+        // Close connection
+        close(connfd);
+
+        if (cancelled) {
+            break;
         }
     }
-    print_statistics();
-    close(listenfd);
+    // Print histogram before exiting server
+    for (int i = 0; i < 95; i++) {
+        char c = i + 32;
+        printf("char '%c' : %u times\n", c, total_PCC[i]);
+    }
     return 0;
 }
